5c9c346a3272f994339f5302a8a9781bce33f364
ellakuser <ellakuser@dit2pc16>
2014-10-08 18:14:28 +0300

    second test commit

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,51 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+var app = angular.module('starter', ['ionic'])
+        .run(function ($ionicPlatform) {
+            $ionicPlatform.ready(function () {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                if (window.cordova && window.cordova.plugins.Keyboard) {
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                }
+                if (window.StatusBar) {
+                    StatusBar.styleDefault();
+                }
+            });
+        });
+
+app.controller('starter', ['$scope', function ($scope) {
+
+        $scope.myMarkers = [];
+
+        $scope.mapOptions = {
+            center: new google.maps.LatLng(35.784, -78.670),
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        $scope.addMarker = function ($event, $params) {
+            $scope.myMarkers.push(new google.maps.Marker({
+                map: $scope.myMap,
+                position: $params[0].latLng
+            }));
+        };
+
+        $scope.setZoomMessage = function (zoom) {
+            $scope.zoomMessage = 'You just zoomed to ' + zoom + '!';
+            console.log(zoom, 'zoomed')
+        };
+
+        $scope.openMarkerInfo = function (marker) {
+            $scope.currentMarker = marker;
+            $scope.currentMarkerLat = marker.getPosition().lat();
+            $scope.currentMarkerLng = marker.getPosition().lng();
+            $scope.myInfoWindow.open($scope.myMap, marker);
+        };
+
+        $scope.setMarkerPosition = function (marker, lat, lng) {
+            marker.setPosition(new google.maps.LatLng(lat, lng));
+        };
+    }]);
